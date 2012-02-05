f6917ae33daea0675ff09f2eb67c59f49cc13332
Marin Usalj <mneorr@gmail.com>
2012-02-05 14:11:44 +0100

    Added the default TableView delegate and datasource snippet

---
+++ delDat.m
@@ -0,0 +1,34 @@
+// UITableViewDelegate and UITableViewDatasource
+// Placeholders for the core table view delegate and datasource methods
+// 
+// Platform: iOS
+// Language: Objective-C
+// Completion Scope: Class Implementation
+
+#pragma mark - Tableview delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    <#action after pressing on the cell#>
+}
+
+
+#pragma mark - Tableview datasource
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *CellIdentifier = @"Cell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:<#(UITableViewCellStyle)#> reuseIdentifier:CellIdentifier];
+    }
+    
+    return cell;
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return <#numberOfSections#>;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return <#numberOfRows#>;
+}
