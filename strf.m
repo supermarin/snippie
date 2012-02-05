47ead8fdaa9b8ee85832aa8b1fc6d17a0e90eebc
Marin Usalj <mneorr@gmail.com>
2012-02-05 16:24:25 +0100

    Added the String with format snippet

---
+++ strf.m
@@ -0,0 +1,9 @@
+// NSString with format
+// A quicker way to do NSString with format and arguments. Works inline
+//
+// Platform: All
+// Language: Objective-C
+// Scope: Code Expression
+
+[NSString stringWithFormat:@"<#your string and %@'s here#>", <#arguments#>]
+
