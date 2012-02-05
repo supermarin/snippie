e47d41c0ca78f4728f56e6a6a39c5630031e9882
Marin Usalj <mneorr@gmail.com>
2012-02-05 14:20:57 +0100

    Added the pragma mark with dynamic delegate name

---
+++ delpragma.m
@@ -0,0 +1,9 @@
+// #pragma mark - Custom delegate
+// Dividers and labels to organize your code into sections
+// This pragma mark adds the dynamic delegate name after the dash
+//
+// Platform: All
+// Language: Objective-C
+// Completion Scopes: Top Level, Class Implementation, Class Interface Methods
+
+#pragma mark - <#delegate name#> delegate
