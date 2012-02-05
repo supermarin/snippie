e9d3f22aaf652c62f5b887122838c364e4e15cbf
Marin Usalj <mneorr@gmail.com>
2012-02-05 15:08:07 +0100

    Added the quicker NSLog snippet

---
+++ nslog.m
@@ -0,0 +1,8 @@
+// NSLog snippet
+// A quicker way to NSLog your stuff
+// 
+// Platform: All
+// Language: Objective-C
+// Completion Scope: Function or Method
+
+NSLog(@"<#your text and %@'s here#>", <#variables here#>);
