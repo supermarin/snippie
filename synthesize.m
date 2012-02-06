122f6d657da199d610be40de86c9eeaefd2d7650
Marin Usalj <mneorr@gmail.com>
2012-02-07 00:43:22 +0100

    Added the synthesize with _prefix snippet

---
+++ synthesize.m
@@ -0,0 +1,9 @@
+// @Synthesizer with _prefix
+// Synthesize your properties quickly with property = _property
+//
+// Platform: All
+// Language: Objective-C
+// Completion Scopes:Class Implementation
+
+@synthesize <#property#> = _<#property#>;
+
