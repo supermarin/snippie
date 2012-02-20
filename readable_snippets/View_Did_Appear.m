0ca5e2a4275f5c244cbd123dd24a877de4bf6277
Marin Usalj <mneorr@gmail.com>
2012-02-20 21:13:38 +0100

    Added some snippets

---
+++ readable_snippets/View_Did_Appear.m
@@ -0,0 +1,11 @@
+//View Did Appear
+//Quickly insert that piece of code!
+//
+//Completion scopes: ["ClassImplementation"]
+//
+Copy / Paste in Xcode:
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+
+}
---
+++ readable_snippets/View_did_dissapear.m
@@ -0,0 +1,11 @@
+//View did dissapear
+//Quick declaration
+//
+//Completion scopes: ["ClassImplementation"]
+//
+Copy / Paste in Xcode:
+
+-(void) viewDidDisappear:(BOOL)animated {
+ 
+    [super viewDidDisappear:animated];
+}
